7c900afd52039cc0fb6aa51ff790c6a7d94d96c9
Shenol Osman <7887878+shenolosman@users.noreply.github.com>
2021-11-24 16:48:29 +0100

    added methods into adminfrontend.

--- FoodRescue-Projekt/AdminFrontend.cs
+++ FoodRescue-Projekt/AdminFrontend.cs
@@ -3,10 +3,63 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DataLayer;
+using DataLayer.Backend;
 
 namespace FoodRescue_Projekt
 {
-    internal class AdminFrontend
+    public class AdminFrontend
     {
+        /*
+         * resetta databasen
+         * titta på alla användare
+         * titta på alla resturanger
+         * knappa in info för, och lägga till en ny restaurang
+         */
+
+        public void ResetDatabase()
+        {
+            AdminBackend.PrepDatabase();
+            Console.WriteLine("Database initialized!");
+            Console.ReadLine();
+            Console.Clear();
+        }
+        public void AllUser()
+        {
+            var users = AdminBackend.AllUsers();
+            foreach (var user in users)
+                Console.WriteLine(user);
+        }
+        public void AllRestaurant()
+        {
+            var restaurants = AdminBackend.ShowRestaurants();
+            foreach (var restaurant in restaurants)
+                Console.WriteLine(restaurant);
+        }
+
+        public void AddRestaurant()
+        {
+            Console.WriteLine("Here we create new restaurant!");
+            Console.Write("Please enter restaurant name:");
+            var restaurantName = Console.ReadLine();
+            Console.Write("Please enter phone nummer:");
+            var phoneNr = Console.ReadLine();
+            Console.Write("Please enter city:");
+            var city = Console.ReadLine();
+            if (!string.IsNullOrWhiteSpace(restaurantName)&&!string.IsNullOrWhiteSpace(restaurantName)&&!string.IsNullOrWhiteSpace(restaurantName))
+            {
+                var add = AdminBackend.AddSpecificRestaurant(restaurantName, phoneNr, city);
+                if (add!=null)
+                {
+                    Console.WriteLine("You have created new restaurant");
+                }
+                else
+                {
+                    Console.WriteLine("Ooops! Something went wrong! Please try again!");
+                }
+            }
+            
+        }
+
     }
 }
--- FoodRescue-Projekt/Program.cs
+++ FoodRescue-Projekt/Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+using FoodRescue_Projekt;
+
 Console.WriteLine("Hello, World!");
+AdminFrontend admin=new AdminFrontend();
+//admin.ResetDatabase();
+//admin.AllUser();
+//admin.AddRestaurant();
+//admin.AllRestaurant();
