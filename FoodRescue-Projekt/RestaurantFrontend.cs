b481d4d8df5abf3b59bf806cf8ff7a86e4540fd2
hannahansson <89410611+hannahansson@users.noreply.github.com>
2021-11-25 19:09:23 +0100

    Fixat #13 i RestaurantFrontend
    Det går nu att lägga till valfri ny matlåda med info till en restaurang

--- FoodRescue-Projekt/RestaurantFrontend.cs
+++ FoodRescue-Projekt/RestaurantFrontend.cs
@@ -76,7 +76,7 @@ public class RestaurantFrontend
                     {
                         break;
                     }
-                } while (cki.Key is not (ConsoleKey.D1 or ConsoleKey.D2 or ConsoleKey.Escape));
+                } while (cki.Key is not (ConsoleKey.D1 or ConsoleKey.D2 or ConsoleKey.D3 or ConsoleKey.Escape));
 
 
                 if (cki.Key == ConsoleKey.D1)
@@ -105,47 +105,91 @@ public class RestaurantFrontend
 
                         Console.ReadKey();
                         Console.WriteLine("Press BACKSPACE to go back");
-                    } while (cki.Key is not ((ConsoleKey.D1 or ConsoleKey.D2 or ConsoleKey.Escape)));
+                    } while (cki.Key is not ((ConsoleKey.D1 or ConsoleKey.D2 or ConsoleKey.D3 or ConsoleKey.Escape)));
                 }
 
                 if (cki.Key == ConsoleKey.D2)
                 {
-                        do
-                        {
-                            Console.Clear();
-                            Console.WriteLine("|\tAll restaurants:\t|");
-                            var restaurants = AdminBackend.ShowRestaurants();
-                            foreach (var r in restaurants)
-                                Console.WriteLine(
-                                    $"| Restaurant: {r.RestaurantName} |\tPhone number: {r.PhoneNumber} |\tCity: {r.City}\t|");
+                    do
+                    {
+                        Console.Clear();
+                        Console.WriteLine("|\tAll restaurants:\t|");
+                        var restaurants = AdminBackend.ShowRestaurants();
+                        foreach (var r in restaurants)
+                            Console.WriteLine(
+                                $"| Restaurant: {r.RestaurantName} |\tPhone number: {r.PhoneNumber} |\tCity: {r.City}\t|");
 
-                            Console.WriteLine("|\tEnter a restaurant you want to see unsold lunch boxes for:\t|");
-                            var restaurant = Console.ReadLine();
-                            Console.Clear();
-                            var restaurantsList = RestaurantBackend.AllUnsoldFoodBoxes(restaurant);
-                            if (restaurantsList.Count > 0)
+                        Console.WriteLine("|\tEnter a restaurant you want to see unsold lunch boxes for:\t|");
+                        var restaurant = Console.ReadLine();
+                        Console.Clear();
+                        var restaurantsList = RestaurantBackend.AllUnsoldFoodBoxes(restaurant);
+                        if (restaurantsList.Count > 0)
+                        {
+                            foreach (var box in restaurantsList)
                             {
-                                foreach (var box in restaurantsList)
-                                {
-                                    Console.WriteLine(
-                                        $" | {box.FoodType} \t| {box.FoodBox} \t| {box.Price}kr \t|");
-                                }
+                                Console.WriteLine(
+                                    $" | {box.FoodType} \t| {box.FoodBox} \t| {box.Price}kr \t|");
+                            }
+                        }
+
+                        Console.ReadKey();
+                        Console.WriteLine("Press BACKSPACE to go back");
+                    } while (cki.Key is not ((ConsoleKey.D1 or ConsoleKey.D2 or ConsoleKey.D3 or ConsoleKey.Escape)));
+                }
+
+                if (cki.Key == ConsoleKey.D3)
+                {
+                    do
+                    {
+                        Console.Clear();
+                        Console.WriteLine("\t            All restaurants:     \t");
+                        var restaurants = AdminBackend.ShowRestaurants();
+                        foreach (var r in restaurants)
+                            Console.WriteLine(
+                                $"| Restaurant: {r.RestaurantName}, Phone number: {r.PhoneNumber}, City: {r.City}|");
+
+                        Console.WriteLine("\n\tEnter a restaurant you want to add a new lunch box to:\t");
+                        var restaurant = Console.ReadLine();
+                        Console.WriteLine("\tEnter food type for the food box [Fisk/Kött/Vego]:\t");
+                        var foodType = Console.ReadLine();
+                        Console.WriteLine("\tEnter dish for the food box:\t");
+                        var foodBox = Console.ReadLine();
+                        Console.WriteLine("\tEnter price for the food box:\t");
+                        var price = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("\tEnter expiry date for the food box:\t");
+                        var expiryDate = Convert.ToDateTime(Console.ReadLine());
+
+                        Console.Clear();
+
+                        var newFoodBoxAdded =
+                            RestaurantBackend.AddNewFoodBoxToAnExistingRestaurant(restaurant, foodType, foodBox,
+                                price, expiryDate);
+                        
+                            if (newFoodBoxAdded != null)
+                            {
+                                Console.WriteLine("New food box was added to the restaurant");
+                            }
+                            else
+                            {
+                                Console.WriteLine("You were not able to add a new food box, Please try again!");
                             }
 
                             Console.ReadKey();
                             Console.WriteLine("Press BACKSPACE to go back");
-                        } while (cki.Key is not ((ConsoleKey.D1 or ConsoleKey.D2 or ConsoleKey.Escape)));
+                    }
+                    while (cki.Key is not ((ConsoleKey.D1 or ConsoleKey.D2 or ConsoleKey.D3
+                           or ConsoleKey.Escape))) ;
                 }
                 else
                 {
-                        Console.ForegroundColor = ConsoleColor.Red;
-                        Console.ResetColor();
-                        Console.WriteLine("Try again later...");
-                        Thread.Sleep(1000);
-                        break;
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.ResetColor();
+                            Console.WriteLine("Try again later...");
+                            Thread.Sleep(1000);
+                            break;
                 }
-            }
-            while (cki.Key != ConsoleKey.Backspace) ;
+            } while (cki.Key != ConsoleKey.Backspace);
         }
     }
 }
+    
