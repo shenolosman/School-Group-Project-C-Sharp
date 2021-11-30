f3c762836cc4e086cea2fdaf91df3bede8f47101
Herman Brunberg <herman.brunberg@utb.ecutbildning.se>
2021-11-30 14:33:55 +0100

    [RED] ClientFrintendTest

---
+++ BackendTests/CustomerClientTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using DataLayer.Backend;
+using Xunit;
+
+namespace BackendTests
+{
+    public class CustomerClientTests
+    {
+       public AdminBackend adminBackend = new AdminBackend();
+
+        [Fact]
+        public void ClientFrontendTest()
+        {
+
+            adminBackend.PrepDatabase();
+
+            var UserList = AdminBackend.AllUsers();
+            var UserNameList = new List<string>();
+            string username = "User";
+            bool loggedin;
+            foreach (var user in UserList)
+            {
+                UserNameList.Add(user.Username);
+            }
+            loggedin = UserNameList.Contains(username);
+            Assert.True(loggedin);
+        }
+        
+    }
+}
